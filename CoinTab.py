ddc1130bf63e83f4c49ce8cfb770b8441b9ea542
SUMIT-JADHAV-23 <sam.jadhav.work@gmail.com>
2024-01-15 11:06:27 +0530

    load data

---
+++ CoinTab.py
@@ -0,0 +1,18 @@
+# import pandas as pd
+# df=pd.read_excel(r"E:\Study sumit\Interviwe Assignments\CoinTab\Courier Company - Rates")
+# print(df)
+import pandas as pd
+#load Left Hand Side (LHS) Data
+order_report = r"E:\Study sumit\Interviwe Assignments\CoinTab\Company X - Order Report.xlsx"
+pincode_zones=r"E:\Study sumit\Interviwe Assignments\CoinTab\Company X - Pincode Zones.xlsx"
+sku_master=r"E:\Study sumit\Interviwe Assignments\CoinTab\Company X - SKU Master.xlsx"
+
+ord = pd.read_excel(order_report)
+pzd= pd.read_excel(pincode_zones)
+smd= pd.read_excel(sku_master)
+print(ord)
+print(pzd)
+print(smd)
+
+
+
