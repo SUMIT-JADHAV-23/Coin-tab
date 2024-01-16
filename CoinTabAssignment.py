dc4aa9c409a0d522375329fbe822ca206f21ee58
SUMIT-JADHAV-23 <sam.jadhav.work@gmail.com>
2024-01-16 11:29:42 +0530

    total_weight_per_order

--- CoinTabAssignment.py
+++ CoinTabAssignment.py
@@ -12,36 +12,37 @@ sku_master_data= pd.read_excel(sku_master)
 Invoicer_data=pd.read_excel(Invoicer)
 Rates_data=pd.read_excel(Rates)
 
-
 #load And merge Left Hand Side (LHS) Data
 #merge data on comman indentifier #sku
 order_sku_data = pd.merge(order_report_data, sku_master_data, on='SKU', how='inner')
-print(order_sku_data)
+# print(order_sku_data)
 
 order_sku_data['Total Weight(g)'] = order_sku_data['Weight (g)'] * order_sku_data['Order Qty']
 
 order_sku_data["Total Weight(g)"]=order_sku_data["Total Weight(g)"]/1000
 order_sku_data["Total Weight(g)"]=order_sku_data["Total Weight(g)"].round(3)
 order_sku_data.rename(columns={"Total Weight(g)": "Total Weight(kg)"}, inplace=True)
-print(order_sku_data)
+# print(order_sku_data)
+
+# file_path= r"E:\Study sumit\Interviwe Assignments\CoinTab\order_sku_data.xlsx"
+# order_sku_data.to_excel(file_path, index=False)
+# print(f"Excel file saved to {file_path}")
+# print(order_sku_data.columns)
+
+order_sku_data.drop(columns=['SKU', 'Order Qty', 'Weight (g)'],inplace=True)
+# print(order_sku_data)
 
 
-# #RHS
-#
-# Courier_Invoicer=r"E:\Study sumit\Interviwe Assignments\CoinTab\Courier Company - Invoice.xlsx"
-# Courier_Rates=r"E:\Study sumit\Interviwe Assignments\CoinTab\Courier Company - Rates.xlsx"
-#
-# Courier_Invoicer_data=pd.read_excel(Invoicer)
-# Courier_Rates_data=pd.read_excel(Rates)
-# # print(Courier_Invoicer_data.dtypes)
-# # print(Courier_Rates_data.dtypes)
-#
-# Courier_Rates_data["Zone"]=Courier_Rates_data["Zone"].str.lower()
-# # print(Courier_Rates_data)
-# Courier_data=pd.merge ( Courier_Invoicer_data, Courier_Rates_data, on="Zone", how="inner")
-# print(Courier_data)
-#
-#
-# new1= r"E:\Study sumit\Interviwe Assignments\CoinTab\Courierdatacombine.xlsx"
-# Courier_data.to_excel(new1, index=False)
-# print(f"Excel file saved to {new1}")
+
+
+# Group by 'Order ID' and calculate the total weight
+total_weight_per_order = order_sku_data.groupby('ExternOrderNo')['Total Weight(kg)'].sum().reset_index()
+
+print(total_weight_per_order)
+
+
+
+file_path= r"E:\Study sumit\Interviwe Assignments\CoinTab\total_weight_per_order.xlsx"
+total_weight_per_order.to_excel(file_path, index=False)
+print(f"Excel file saved to {file_path}")
+
